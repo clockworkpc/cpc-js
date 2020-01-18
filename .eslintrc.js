a6468ef7a24e662b51483782079d6431b0e77ee0
alexander <agarber@profectusgroup.com>
2020-01-18 17:42:51 +1100

    configuration

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "env": {
+    "node": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "no-empty": "error",
+    "no-multiple-empty-lines": "warn",
+    "no-var": "error",
+    "prefer-const": "error",
+    "no-undef": "warn"
+  },
+  "globals": {
+    "describe": true,
+    "context": true,
+    "it": true
+  }
+};
